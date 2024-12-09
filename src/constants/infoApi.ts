3ea0809f0fe5a394c10ddce894864046b1a1e82b
PhatIT2003 <banconcop113@gmail.com>
2024-12-09 14:39:03 +0700

    aa

--- src/constants/infoApi.ts
+++ src/constants/infoApi.ts
@@ -1,1 +1,3 @@
-export const BASE_URL="https://backend-fakeapi-sg7d.onrender.com"
+// export const BASE_URL="https://backend-fakeapi-sg7d.onrender.com"
+
+export const BASE_URL="https://backendfakeapi-production.up.railway.app"
